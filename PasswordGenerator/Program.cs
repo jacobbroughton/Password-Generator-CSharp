ad5330e7571cd89ff09a1c5ddde141c4c287a757
jlb1999 <jlbroughton88@gmail.com>
2020-03-16 08:31:21 -0400

    Added prompts, better logic for randomizer

--- PasswordGenerator/Program.cs
+++ PasswordGenerator/Program.cs
@@ -12,27 +12,57 @@ namespace PasswordGenerator
     class Program
     {
 
-        public void Randomize()
+        public void Prompt()
+        {
+            Console.WriteLine("--------------------");
+            Console.Write("Alphabet characters? (y/n) ");
+            string pAlph = Console.ReadLine();
+            Console.WriteLine("--------------------");
+            Console.Write("Integers? (y/n) ");
+            string pNum = Console.ReadLine();
+            Console.WriteLine("--------------------");
+            Console.Write("Symbols? (y/n) ");
+            string pSym = Console.ReadLine();
+            Console.WriteLine("--------------------");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("--------------------");
+
+            Program p = new Program();
+            p.Randomize(pAlph, pNum, pSym);
+        }
+
+        public void Randomize(string alphSig, string numSig, string symSig)
         {
             Random random = new Random(); // Creates random object
-            ArrayList alphList = new ArrayList();
-            ArrayList numList = new ArrayList();
+            List<char[]> charList = new List<char[]>();
 
             int randomNumInt = random.Next(); // 10 digit randomized int
             string randomNumStr = randomNumInt.ToString(); 
             string alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            string alphNumCombo = randomNumStr + alph;
-            Console.WriteLine(alphNumCombo);
+            string symbols = "{}_-!?[]";
             char[] alphArr = alph.ToCharArray();
+            char[] numArr = randomNumStr.ToCharArray();
+            char[] symbolArr = symbols.ToCharArray();
 
-            alphList.Add(alph);
-            numList.Add(randomNumStr);
+            charList.Add(alphArr);
+            charList.Add(numArr);
+            charList.Add(symbolArr);
+
+           for(int i = 0; i < charList.Count; i++)
+            {
+                Console.WriteLine(charList[i]);
+            }
+
+           Console.WriteLine(alphSig);
+            Console.WriteLine(numSig);
+            Console.WriteLine(symSig);
         }
 
         static void Main(string[] args)
         {
             Program p = new Program();
-            p.Randomize();
+            p.Prompt();
+            //p.Randomize();
         }
     }
 }
