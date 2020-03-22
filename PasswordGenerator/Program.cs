3ccc9cf03276450ace32f4be0b11fc2eecca383e
jlb1999 <jlbroughton88@gmail.com>
2020-03-22 08:12:04 -0400

    Finished randomize functionality, returns password

--- PasswordGenerator/Program.cs
+++ PasswordGenerator/Program.cs
@@ -12,57 +12,84 @@ namespace PasswordGenerator
     class Program
     {
 
+        // Prompt
         public void Prompt()
         {
-            Console.WriteLine("--------------------");
-            Console.Write("Alphabet characters? (y/n) ");
-            string pAlph = Console.ReadLine();
-            Console.WriteLine("--------------------");
-            Console.Write("Integers? (y/n) ");
-            string pNum = Console.ReadLine();
-            Console.WriteLine("--------------------");
-            Console.Write("Symbols? (y/n) ");
-            string pSym = Console.ReadLine();
-            Console.WriteLine("--------------------");
-            Console.WriteLine("--------------------");
-            Console.WriteLine("--------------------");
+            int charNum;
+            string pAlph;
+            string pNum;
+            string pSym;
+            bool alphComplete;
+            bool numComplete;
+            bool symComplete;
+
+            (int, string, string, string) InnerPrompt ()
+            {
+                Console.Write("How many characters in the password? ");
+                charNum = Convert.ToInt32((Console.ReadLine()));
+
+                Console.Write("Alphabet characters? (y/n) ");
+                pAlph = Console.ReadLine();
+
+                Console.Write("Integers? (y/n) ");
+                pNum = Console.ReadLine();
+
+                Console.Write("Symbols? (y/n) ");
+                pSym = Console.ReadLine();
+
+                return (charNum, pAlph, pNum, pSym);
+            }
+            InnerPrompt();
+
+            alphComplete = (pAlph == "y" || pAlph == "n") ? true : false;
+            numComplete = (pNum == "y" || pNum == "n") ? true : false;
+            symComplete = (pSym == "y" || pSym == "n") ? true : false;
 
             Program p = new Program();
-            p.Randomize(pAlph, pNum, pSym);
+
+            if(alphComplete && numComplete && symComplete)
+            {
+                p.Randomize(charNum, pAlph, pNum, pSym);
+            } else
+            {
+                InnerPrompt();
+            }
+            
         }
 
-        public void Randomize(string alphSig, string numSig, string symSig)
+
+        // Randomize
+        public void Randomize(int charNum, string alphSig, string numSig, string symSig)
         {
             Random random = new Random(); // Creates random object
-            List<char[]> charList = new List<char[]>();
 
             int randomNumInt = random.Next(); // 10 digit randomized int
             string randomNumStr = randomNumInt.ToString(); 
-            string alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            string symbols = "{}_-!?[]";
-            char[] alphArr = alph.ToCharArray();
-            char[] numArr = randomNumStr.ToCharArray();
-            char[] symbolArr = symbols.ToCharArray();
+            string alphStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string symbolsStr = "{}_-!?[]";
+            string combinedStr = "";
 
-            charList.Add(alphArr);
-            charList.Add(numArr);
-            charList.Add(symbolArr);
+            if ( alphSig == "y" ) { combinedStr += alphStr; };
+            if ( numSig == "y" ) { combinedStr += randomNumStr; };
+            if ( symSig == "y" ) { combinedStr += symbolsStr; };
 
-           for(int i = 0; i < charList.Count; i++)
+            string randomStr = "";
+            for(int i = 0; i < charNum; i++)
             {
-                Console.WriteLine(charList[i]);
+                int matchingRand = random.Next(0, combinedStr.Length);
+                randomStr += combinedStr[matchingRand];
             }
-
-           Console.WriteLine(alphSig);
-            Console.WriteLine(numSig);
-            Console.WriteLine(symSig);
+            Console.WriteLine("------------------");
+            Console.WriteLine(" PASSWORD GENERATED");
+            Console.WriteLine("------------------");
+            Console.WriteLine(" " + randomStr);
+            Console.WriteLine("------------------");
         }
 
         static void Main(string[] args)
         {
             Program p = new Program();
             p.Prompt();
-            //p.Randomize();
         }
     }
 }
